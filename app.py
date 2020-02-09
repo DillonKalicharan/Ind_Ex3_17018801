fa6887ba32dba4054b9d57a18acd4852b35719dd
DillonKalicharan <dillon.kalicharan.17@ucl.ac.uk>
2020-02-09 23:09:16 +0000

    Flask App Ind_Ex3_17018801

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
-
+from flask import Flask, render_template, flash, redirect,url_for
+from forms import SignUpForm
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '0123456789'
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        flash(f'It worked :D')
+        return redirect(url_for('index'))
+
+    return render_template('signup.html', form=form)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
